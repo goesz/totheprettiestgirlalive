eda400a71d9bd79985daa0dccd5d7dea8adedf71
Rafael Goes <goesdeveloper@gmail.com>
2025-06-22 00:03:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -104,11 +104,16 @@ function mostrarMotivo() {
   const classes = ["top-left", "top-right", "bottom-left", "bottom-right"];
   fita.className = "fita";
 
-  if (Math.random() < 0.3) {
+  if (Math.random() < 0.3 && modo === 'principais') {
     const aleatoria = classes[Math.floor(Math.random() * classes.length)];
     fita.classList.add(aleatoria);
     fita.style.display = "block";
-  } else {
+  } else if(modo === 'extras') {
+const aleatoria = classes[Math.floor(Math.random() * (classes.length - 1)) + 1];
+    console.log(aleatoria)
+    fita.classList.add(aleatoria);
+    fita.style.display = "block";
+  }  else {
     fita.style.display = "none";
   }
 }
