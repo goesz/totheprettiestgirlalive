1477c16c9044248ba5cf3b998db8bba77b674312
Rafael Goes <goesdeveloper@gmail.com>
2025-06-30 22:13:55 -0300

    bugfix

--- extras.js
+++ extras.js
@@ -4,11 +4,11 @@ const motivosExtras = [
     titulo: '1. Apoio emocional',
     descricao: 'Quando seus pés não alcançarem o chão, saiba que pode contar com os meus.',
     adicionado: '21/06/2025'
-  },
-    {
+  }, /*
+  {
     img: 'extras/2.jpg',
-    titulo: '2. Você nunca mais jogará sozinha',
-    descricao: 'Vai ter um duo sempre que quiser jogar uma partidinha',
+    titulo: '2. Você nunca mais vai jogar sozinha',
+    descricao: 'Vai ter um duo sempre que quiser jogar uma partidinha!',
     adicionado: '30/06/2025'
-  },
+  } */
 ];
